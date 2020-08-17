bc0507a2fccc02624a786d0ce3498f26a26ee8cc
Peter Fearn <peter@cncrrnt.com>
2020-08-17 16:39:28 +0100

    Initial version

---
+++ SubstituteQueryable.Test/Person.cs
@@ -0,0 +1,1 @@
+﻿namespace SubstituteQueryable.Test{	using System.Collections.Generic;	class Person	{		public string Name { get; set; }		public Person Parent { get; set; }		public IEnumerable<Person> Children { get; set; }		public override string ToString()		{			return Name;		}	}}
